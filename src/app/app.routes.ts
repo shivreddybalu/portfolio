bb445d41398798e1dde6ceec94d35f7d70ce3ea9
Shiva Balu <balushivareddy@gmail.com>
2026-06-19 01:10:15 +0530

    title and icon updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,31 +4,31 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    title: 'Shiv Kumar | Angular Developer'
+    title: 'Balu | Angular Developer'
   },
   {
     path: 'about',
     loadComponent: () => import('./pages/about/about-page.component').then(m => m.AboutPageComponent),
-    title: 'About | Shiv Kumar'
+    title: 'About | Balu'
   },
   {
     path: 'skills',
     loadComponent: () => import('./pages/skills/skills-page.component').then(m => m.SkillsPageComponent),
-    title: 'Skills | Shiv Kumar'
+    title: 'Skills | Balu'
   },
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects-page.component').then(m => m.ProjectsPageComponent),
-    title: 'Projects | Shiv Kumar'
+    title: 'Projects | Balu'
   },
   {
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact-page.component').then(m => m.ContactPageComponent),
-    title: 'Contact | Shiv Kumar'
+    title: 'Contact | Balu'
   },
   {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent),
-    title: '404 | Shiv Kumar'
+    title: '404 | Balu'
   }
 ];
